75fbf7495a42ed8d8e5d58a164a40c99cf8de443
Mark Holm Justesen <spammailgg25@gmail.com>
2021-04-15 19:17:54 +0200

    Added register form + parent to child example

---
+++ client/src/app/register/register.component.ts
@@ -0,0 +1,25 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+  @Input() usersFromHomeComponent: any;
+  @Output() cancelRegiser = new EventEmitter();
+  model: any = {};
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  register() {
+    console.log(this.model);
+  }
+
+  cancel() {
+    this.cancelRegiser.emit(false);
+  }
+}
