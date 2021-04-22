e0ab5b2f3a05012886c094179a952c69ab1237dc
Mark Holm Justesen <spammailgg25@gmail.com>
2021-04-22 22:20:42 +0200

    Added Repo Pattern, Automapper & DTOs

---
+++ API/Helpers/AutoMapperProfiles.cs
@@ -0,0 +1,24 @@
+using System.Linq;
+using API.DTOs;
+using API.Entities;
+using API.Extensions;
+using AutoMapper;
+
+namespace API.Helpers
+{
+    public class AutoMapperProfiles : Profile
+    {
+        public AutoMapperProfiles()
+        {
+            CreateMap<AppUser, MemberDto>()
+                // Gets the photo url from the main photo
+                .ForMember(dest => dest.PhotoUrl, opt => opt
+                    .MapFrom(src => src.Photos.FirstOrDefault(x => x.IsMain).Url))
+                // Gets the age from CalculateAge method
+                .ForMember(dest => dest.Age, opt => opt
+                    .MapFrom(src => src.DateOfBirth.CalculateAge()));
+
+            CreateMap<Photo, PhotoDto>();
+        }
+    }
+}
