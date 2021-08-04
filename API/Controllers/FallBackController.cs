fd7cfd71d5b7246bf0e62c65e15ea7bcb9b3c8f6
Mark Holm Justesen <spammailgg25@gmail.com>
2021-08-04 08:51:37 +0200

    Publish to heroku

--- API/Controllers/FallBackController.cs
+++ API/Controllers/FallBackController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
-    public class FallBackController : Controller
+    public class FallbackController : Controller
     {
         public ActionResult Index()
         {
